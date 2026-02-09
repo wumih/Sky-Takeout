2426dd4a368bb8c20b310271b4972d8d612399e1
wumih <120448608+wumih@users.noreply.github.com>
2026-02-09 22:27:45 +0800

    修复新增员工的创始人ID 和 其他值被设定为固定值的 错误

--- sky-common/src/main/java/com/sky/context/BaseContext.java
+++ sky-common/src/main/java/com/sky/context/BaseContext.java
@@ -1,19 +1,21 @@
 package com.sky.context;
 
 public class BaseContext {
-
+    // 创建一个 ThreadLocal，专门用来存 Long 类型的用户ID
     public static ThreadLocal<Long> threadLocal = new ThreadLocal<>();
 
+    // 1. 存数据 (工人把数据写进自己的本子)
     public static void setCurrentId(Long id) {
         threadLocal.set(id);
     }
 
+    // 2. 取数据 (工人从自己的本子上读数据)
     public static Long getCurrentId() {
         return threadLocal.get();
     }
 
+    // 3. 清理数据 (把本子擦干净，防止内存泄漏)
     public static void removeCurrentId() {
         threadLocal.remove();
     }
-
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -3,6 +3,7 @@ package com.sky.service.impl;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
@@ -82,8 +83,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateTime(LocalDateTime.now());
 
         //设置当前记录创建人id和修改人id
-        employee.setCreateUser(10L);//目前写个假数据，后期修改
-        employee.setUpdateUser(10L);
+        employee.setCreateUser(BaseContext.getCurrentId());
+        employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);//后续步骤定义
     }
