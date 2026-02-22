68b6e32d56b83ef94d673bf52f67914e3b9d1839
wumih <120448608+wumih@users.noreply.github.com>
2026-02-22 19:59:45 +0800

    增加了用户下单以及订单支付功能注意因为没有微信对应的 付款资格所以说采用的是模拟支付的方式

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -20,4 +20,13 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+    /**
+     * 根据主键查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{id}")
+    User getById(Long userId);
+
+
 }
